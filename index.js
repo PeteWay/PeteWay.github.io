8c68527865eba5bcce56f06a2ed8020f3f8fcdd1
oitsjustjose <stovallj1995@gmail.com>
2019-05-08 11:43:05 -0400

    Initial Commit

---
+++ index.js
@@ -0,0 +1,20 @@
+// This is for the slide-down menu up top!
+$(document)
+    .ready(function ()
+    {
+        $('.masthead').visibility({
+            once: false,
+            onBottomPassed: function ()
+            {
+                $('.fixed.menu').transition('fade in');
+            },
+            onBottomPassedReverse: function ()
+            {
+                $('.fixed.menu').transition('fade out');
+            }
+        });
+
+        $('.ui.sidebar').sidebar('attach events', '.toc.item');
+
+
+    });
