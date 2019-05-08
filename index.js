57f338e5d8210e0bf838a8a75d9da49133b31bb7
oitsjustjose <stovallj1995@gmail.com>
2019-05-08 12:47:26 -0400

    Update!

--- index.js
+++ index.js
@@ -1,20 +1,17 @@
-// This is for the slide-down menu up top!
 $(document)
-    .ready(function ()
-    {
+    .ready(function () {
+        // This is for the slide-down menu up top!
         $('.masthead').visibility({
             once: false,
-            onBottomPassed: function ()
-            {
+            onBottomPassed: function () {
                 $('.fixed.menu').transition('fade in');
             },
-            onBottomPassedReverse: function ()
-            {
+            onBottomPassedReverse: function () {
                 $('.fixed.menu').transition('fade out');
             }
         });
-
+        // Attach the event
         $('.ui.sidebar').sidebar('attach events', '.toc.item');
-
-
+        // Embed all the "ui embed" classes
+        $(".ui.embed").embed();
     });
